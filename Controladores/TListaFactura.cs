d347d056cf56463c6b74232d2c3dd1736ad00724
cxrlossolorzano120-gif <cxrlossolorzano120@gmail.com>
2026-06-03 08:47:44 -0500

    TlistaFactura

--- Controladores/TListaFactura.cs
+++ Controladores/TListaFactura.cs
@@ -58,7 +58,6 @@ namespace RelacionesEntreClases_Mendoza.Controladores
                     lista.Add(TListaFactura.Lista_Factura[i]);
                 }
             }
-
             return lista;
         }
     }
